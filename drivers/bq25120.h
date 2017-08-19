810ef9945b4aa51c2c82739a625111bee251b948
Thomas J D Dawkins <tjdawkins@redtreeit.com>
2017-08-19 14:39:23 -0700

    Create header for BQ25120 drivers

---
+++ drivers/bq25120.h
@@ -0,0 +1,22 @@
+/*
+ * bq25120.h
+ *
+ * Drivers for the TI BQ25120 PMIC
+ *
+ *  Created on: Aug 19, 2017
+ *      Author: tdawkins
+ */
+
+#ifndef DRIVERS_BQ25120_H_
+#define DRIVERS_BQ25120_H_
+
+
+// Define Memory Addresses
+#define ADDR_STATUS_SHIP_MODE 0x00
+#define ADDR_FAULTS_FAULT_MASK 0x01
+//TODO: More
+
+
+
+
+#endif /* DRIVERS_BQ25120_H_ */
