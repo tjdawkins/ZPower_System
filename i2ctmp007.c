7c1d9f38a3b0ce60e99f6816d210018fa98b29a5
TJEDII <tjdawkins@redtreeit.com>
2017-08-24 08:25:05 -0700

    Move chip initialization to driver

--- i2ctmp007.c
+++ i2ctmp007.c
@@ -51,94 +51,26 @@
 #define TMP007_DIE_TEMP     0x0001  /* Die Temp Result Register */
 #define TMP007_OBJ_TEMP     0x0003  /* Object Temp Result Register */
 
-static Display_Handle display;
+//static Display_Handle display;
 
 /*
  *  ======== mainThread ========
  */
-void *mainThread(void *arg0)
-{
-    unsigned int    i;
-    uint8_t         reg[BQ25120_NUM_REGS];
-    uint8_t         txBuffer[1];
-    uint8_t         rxBuffer[1];
-    I2C_Handle      i2c;
-    I2C_Params      i2cParams;
-    I2C_Transaction i2cTransaction;
+void *mainThread(void *arg0) {
 
     /* Call driver init functions */
     Display_init();
-    GPIO_init();
-    I2C_init();
+    //GPIO_init();
+    //I2C_init();
 
-    /* Open the HOST display for output */
+    /*/ Open the HOST display for output
     display = Display_open(Display_Type_UART, NULL);
     if (display == NULL) {
         while (1);
-    }
+    }*/
 
-    /* Turn on user LED */
-    GPIO_write(Board_GPIO_LED0, Board_GPIO_LED_ON);
-    Display_printf(display, 0, 0, "Starting the i2ctmp007 example\n");
+    BQ25120_init();
 
-    /* Create I2C for usage */
-    I2C_Params_init(&i2cParams);
-    i2cParams.bitRate = I2C_400kHz;
+    return(NULL);
 
-    i2c = I2C_open(Board_I2C_TMP, &i2cParams);
-    if (i2c == NULL) {
-        Display_printf(display, 0, 0, "Error Initializing I2C\n");
-        while (1);
-    }
-    else {
-        Display_printf(display, 0, 0, "I2C Initialized!\n");
-    }
-
-    /* Point to the T ambient register and read its 2 bytes */
-    txBuffer[0] = 0x00;
-    i2cTransaction.slaveAddress = BQ25120_I2C_ADDR;
-    i2cTransaction.writeBuf = txBuffer;
-    i2cTransaction.writeCount = 1;
-    i2cTransaction.readBuf = rxBuffer;
-    i2cTransaction.readCount = 1;
-
-    /* Take 20 samples and print them out onto the console */
-    for (i = 0; i < BQ25120_NUM_REGS; i++) {
-        if (I2C_transfer(i2c, &i2cTransaction)) {
-            /* Extract degrees C from the received data; see TMP102 datasheet */
-            reg[i] = rxBuffer[0];
-            Display_printf(display, 0, 0, "Register 0x%02x: 0x%02x\n", i, reg[i]);
-
-        }
-        else {
-            Display_printf(display, 0, 0, "I2C Bus fault\n");
-        }
-
-        txBuffer[0]++;
-        /* Sleep for 1 second */
-        sleep(1);
-    }
-
-
-    // Init Function Prototype
-
-    i2cTransaction.slaveAddress = BQ25120_I2C_ADDR;
-    i2cTransaction.writeBuf = txBuffer;
-    i2cTransaction.writeCount = 2;
-    i2cTransaction.readBuf = rxBuffer;
-    i2cTransaction.readCount = 1;
-
-    //txBuffer[0] = ADDR_FASTCHGCTRL;
-    //txBuffer[1] = FC_CHRG_ENABLE | FC_HIGHZ_DISABLE | FC_ICHRG_CODE | FC_ICHRG_RANGE | FAULT_BAT_OCP_M;
-
-    txBuffer[0] = ADDR_SYSVOUT;
-    txBuffer[1] = SYS_VOUT_CODE | SYS_VOUT_ENABLE | SYS_VOUT_SEL;
-
-    if(I2C_transfer(i2c, &i2cTransaction))
-        Display_printf(display, 0, 0, "FC Register 0x%02x: 0x%02x\n", txBuffer[0], reg[i]);
-
-    /* Deinitialized I2C */
-    I2C_close(i2c);
-    Display_printf(display, 0, 0, "I2C closed!\n");
-    return (NULL);
 }
